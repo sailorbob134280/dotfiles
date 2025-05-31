c1e9d3a7ebc654b70f31a2774b618db94987d564
Jackson Smith <jackson.smith@loftorbital.com>
2025-05-30 20:41:10 -0600

    Update lspconfig

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,70 +1,80 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-local util = require("lspconfig.util")
+local lspconfig = require "lspconfig"
+local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
+local util = require "lspconfig.util"
 
--- EXAMPLE
 local servers = { "html", "cssls", "svelte", "ts_ls", "tailwindcss" }
 local nvlsp = require "nvchad.configs.lspconfig"
+
+-- merge blink capabilities with nvlsp capabilities
+local capabilities = vim.tbl_deep_extend("force", nvlsp.capabilities, blink_capabilities)
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
+    capabilities = capabilities,
   }
 end
 
-lspconfig.emmet_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
+lspconfig.emmet_ls.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = capabilities,
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "typescriptreact",
+    "templ",
+    "vue",
+  },
+  init_options = {
+    html = {
+      options = {
+        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+        ["bem.enabled"] = true,
       },
-    }
-})
-
-local custom_cmd = {
-  "./coruscant/scripts/run-clangd.sh",
-  "--background-index",
-  "--clang-tidy",
-  "--header-insertion=iwyu",
-  "--completion-style=detailed",
-  "--function-arg-placeholders",
-  "--fallback-style=llvm",
+    },
+  },
 }
 
-local start_script = './coruscant/scripts/run-clangd.sh'
+local custom_cmd = {
+  "./coruscant/run.sh",
+  "--clangd",
+}
+
+local start_script = "./coruscant/run.sh"
 local file_exists = vim.fn.filereadable(start_script) == 1
 
-lspconfig.clangd.setup({
-  on_attach = function (client, bufnr)
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
+    nvlsp.on_attach(client, bufnr)
   end,
   cmd = file_exists and custom_cmd or cmd,
-  capabilities = capabilities
-})
-
-lspconfig.pyright.setup({
-  on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"python"}
-})
+}
+
+lspconfig.pyright.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = capabilities,
+  filetypes = { "python" },
+}
 
 lspconfig.gopls.setup {
-  on_attach = on_attach,
+  on_attach = nvlsp.on_attach,
   capabilities = capabilities,
-  cmd = {"gopls"},
+  cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
